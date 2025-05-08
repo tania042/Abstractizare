4b4b7900faf1b03fdeb2bac0a88091be83b0591e
Tatiana Ceban <tatiana.ceban.19@bk.ru>
2025-05-08 16:26:48 +0300

    Change in Interface class

--- src/Exemplu.java
+++ src/Exemplu.java
@@ -2,6 +2,7 @@ public class Exemplu {
     //acest program incepe cuapelarea metodei main
     public static void main(String[] args) {
         System.out.println("Un simplu program java.");
+        System.out.println("Buna ziua.");
 
     }
 }
